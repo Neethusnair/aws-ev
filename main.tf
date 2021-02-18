0d789a446965cd068e4f41f142f419de2c02ad83
Neethu <neethu.somasekharan@sreyo.com>
2021-02-18 13:05:44 -0800

    first commit

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region     = "us-west-1"
+  #access_key = "******"
+  #secret_key = "*******"
+}
+
+
+resource "aws_instance" "web" {
+  ami           = "ami-00831fc7c1e3ddc60"
+  instance_type = "t2.micro"
+  tag = {
+    Name = "nsubnet"
+  }
+}
